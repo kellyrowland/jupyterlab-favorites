a07451746453dac5627362d64f182e96fa89dc3b
Kelly Rowland <kellylynnerowland@gmail.com>
2024-09-04 15:06:17 -0700

    test

--- src/index.tsx
+++ src/index.tsx
@@ -76,7 +76,7 @@ const favorites: JupyterFrontEndPlugin<IFavorites> = {
       layout.widgets.forEach((widget, index) => {
         console.log(widget.node.className)
         console.log(index)
-        console.log(widget.item)
+        console.log(widget.node.childNodes)
         if (widget.node.className.includes('jp-ToolBar-item')) {
           toolbarItemIndex = index;
         }
